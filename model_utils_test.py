09b0448ed4afa14c9eb9e7e8a1182b3f05ff0464
bontim <timaio@outlook.jp>
2024-06-03 23:09:28 +0900

    utils

---
+++ model_utils_test.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import torch
+from typing import Any, Dict, List, Optional, Tuple, Union
+from torch import nn
+from transformers import DataCollatorForSeq2Seq
+from transformers import Seq2SeqTrainer
+
--- train_utils_test.py
+++ train_utils_test.py
@@ -1,1 +1,11 @@
-import numpy as np
+import os
+import shutil
+import logging
+
+from transformers import Seq2SeqTrainingArguments, Seq2SeqTrainer
+from transformers import T5ForConditionalGeneration
+from transformers import DataCollatorForSeq2Seq
+from transformers.trainer_utils import set_seed
+
+# from model_utils import TaskPrefixDataCollator, TaskPrefixTrainer
+
